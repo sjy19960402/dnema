feb33b54529e77ac0938495ad47c96b64127690c
sjy19960402 <sjy19960402@gmail.com>
2022-11-23 15:34:39 +0900

    add package.js scripts

--- app.js
+++ app.js
@@ -54,19 +54,3 @@ app.get("/intro", function (request, response) {
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
 });
-// =======
-// const express = require("express");
-// const app = express();
-// const PORT = 8000;
-
-// app.set("view engine", "ejs");
-// app.use("/views", express.static(__dirname + "/views"));
-
-// app.get("/", function (req, res) {
-//   res.render("main");
-// });
-
-// app.listen(PORT, function () {
-
-//   console.log(`listening on port ${PORT}! http://localhost:${PORT}`);
-// });
