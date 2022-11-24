c8db30735ef4cb1f39d8338335b829c91db06ca2
shin jinyoung <100466973+sjy19960402@users.noreply.github.com>
2022-11-24 17:55:16 +0900

    Delete intro.js

--- static/js/intro.js
+++
@@ -1,9 +0,0 @@
-// let textO = document.querySelector(".text-o");
-// let textCI = document.querySelector(".text-ci");
-
-// window.addEventListener("load", function () {
-//   $(".text-box").fadeIn(2000);
-//   console.log(textO);
-//   textO.style.transform = "scale(0.5)";
-//   textCI.style.opacity.WebkitAnimationDelay = "3s";
-// });
